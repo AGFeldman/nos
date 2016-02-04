787a1d332a76bcd1c77247638dd2a11763bc2729
Aaron Feldman <feldmando@gmail.com>
2016-02-04 13:23:11 -0800

    rsync scripts cd into their own directories

--- aaron_rsync.sh
+++ aaron_rsync.sh
@@ -1,2 +1,4 @@
-# This should be executed from inside of nos/
+#!/bin/bash
+# cd to the directory of this script
+cd "$(dirname "$0")"
 rsync -zarv --chmod=777 --exclude="*.pdf" --exclude=".git*" --exclude="src/doc/*" --exclude="spec/" . "fire:~/Desktop/aaron_shared_folder"
--- freedom1_rsync.sh
+++ freedom1_rsync.sh
@@ -1,2 +1,4 @@
-# This should be executed from inside of nos/
+#!/bin/bash
+# cd to the directory of this script
+cd "$(dirname "$0")"
 rsync -zarv --chmod=777 --exclude="*.pdf" --exclude=".git*" --exclude="src/doc/*" --exclude="spec/" . "freedom1:~/freedom1_shared_folder"
