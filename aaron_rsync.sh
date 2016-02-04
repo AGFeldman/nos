a9208f90180bbc07211d2952bc0fbd3f264c44c1
Aaron Feldman <feldmando@gmail.com>
2016-02-04 13:08:29 -0800

    Update rsync scripts
    chmod now done with the rsync command

--- aaron_rsync.sh
+++ aaron_rsync.sh
@@ -1,3 +1,2 @@
 # This should be executed from inside of nos/
-rsync -zarvp --exclude="*.pdf" --exclude=".git*" --exclude="src/doc/*" --exclude="spec/" . "fire:~/Desktop/aaron_shared_folder"
-ssh fire "chmod -R 777 /home/aaron/Desktop/aaron_shared_folder"
+rsync -zarv --chmod=777 --exclude="*.pdf" --exclude=".git*" --exclude="src/doc/*" --exclude="spec/" . "fire:~/Desktop/aaron_shared_folder"
--- freedom1_rsync.sh
+++ freedom1_rsync.sh
@@ -1,3 +1,2 @@
 # This should be executed from inside of nos/
-rsync -zarvp --exclude="*.pdf" --exclude=".git*" --exclude="src/doc/*" --exclude="spec/" . "freedom1:~/freedom1_shared_folder"
-ssh freedom1 "chmod -R 777 /home/freedom1/freedom1_shared_folder"
+rsync -zarv --chmod=777 --exclude="*.pdf" --exclude=".git*" --exclude="src/doc/*" --exclude="spec/" . "freedom1:~/freedom1_shared_folder"
