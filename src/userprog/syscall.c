1abbd5ede471605f8102ff4f4753f60b2aed4dfb
Aaron Feldman <feldmando@gmail.com>
2016-02-17 00:00:06 -0800

    syscall_handler() examines syscall number
    Also, process_wait() does an infinite loop and setup_stack() uses `*esp
    = PHYS_BASE - 12;`

--- src/userprog/syscall.c
+++ src/userprog/syscall.c
@@ -10,8 +10,36 @@ void syscall_init(void) {
     intr_register_int(0x30, 3, INTR_ON, syscall_handler, "syscall");
 }
 
-static void syscall_handler(struct intr_frame *f UNUSED) {
-    printf("system call!\n");
+static void syscall_handler(struct intr_frame *f) {
+    // TODO: Finish
+
+    int syscall_num = *((int *) f->esp);
+    if (syscall_num == SYS_HALT) {
+        printf("system call: halt\n");
+    } else if (syscall_num == SYS_EXIT) {
+        printf("system call: exit\n");
+    } else if (syscall_num == SYS_WAIT) {
+        printf("system call: wait\n");
+    } else if (syscall_num == SYS_CREATE) {
+        printf("system call: create\n");
+    } else if (syscall_num == SYS_REMOVE) {
+        printf("system call: remove\n");
+    } else if (syscall_num == SYS_OPEN) {
+        printf("system call: open\n");
+    } else if (syscall_num == SYS_FILESIZE) {
+        printf("system call: filesize\n");
+    } else if (syscall_num == SYS_READ) {
+        printf("system call: read\n");
+    } else if (syscall_num == SYS_WRITE) {
+        printf("system call: write\n");
+    } else if (syscall_num == SYS_SEEK) {
+        printf("system call: seek\n");
+    } else if (syscall_num == SYS_TELL) {
+        printf("system call: tell\n");
+    } else if (syscall_num == SYS_CLOSE) {
+        printf("system call: close\n");
+    } else {
+        printf("system call: not handled!\n");
+    }
     thread_exit();
 }
-
