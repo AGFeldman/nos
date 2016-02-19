e99ad692bb8bcb4dce7d8cf5b2ca4a7f569eb3bc
Aaron Feldman <feldmando@gmail.com>
2016-02-18 17:36:11 -0800

    Call sys_exit_helper() from page fault handler
    Previously, code similar to sys_exit_helper was repeated in page fault
    handler.
    
    sys_exit_helper() also releases the filesys_lock if it is held.
    
    pass tests/filesys/base/syn-write
    pass tests/userprog/args-none
    pass tests/userprog/args-single
    pass tests/userprog/args-multiple
    pass tests/userprog/args-many
    pass tests/userprog/args-dbl-space
    pass tests/userprog/sc-bad-sp
    pass tests/userprog/sc-bad-arg
    pass tests/userprog/sc-boundary
    pass tests/userprog/sc-boundary-2
    pass tests/userprog/halt
    pass tests/userprog/exit
    pass tests/userprog/create-normal
    pass tests/userprog/create-empty
    pass tests/userprog/create-null
    pass tests/userprog/create-bad-ptr
    pass tests/userprog/create-long
    pass tests/userprog/create-exists
    pass tests/userprog/create-bound
    pass tests/userprog/open-normal
    pass tests/userprog/open-missing
    pass tests/userprog/open-boundary
    pass tests/userprog/open-empty
    pass tests/userprog/open-null
    pass tests/userprog/open-bad-ptr
    pass tests/userprog/open-twice
    pass tests/userprog/close-normal
    pass tests/userprog/close-twice
    pass tests/userprog/close-stdin
    pass tests/userprog/close-stdout
    pass tests/userprog/close-bad-fd
    pass tests/userprog/read-normal
    pass tests/userprog/read-bad-ptr
    pass tests/userprog/read-boundary
    pass tests/userprog/read-zero
    pass tests/userprog/read-stdout
    pass tests/userprog/read-bad-fd
    pass tests/userprog/write-normal
    pass tests/userprog/write-bad-ptr
    pass tests/userprog/write-boundary
    pass tests/userprog/write-zero
    pass tests/userprog/write-stdin
    pass tests/userprog/write-bad-fd
    pass tests/userprog/exec-once
    pass tests/userprog/exec-arg
    pass tests/userprog/exec-multiple
    pass tests/userprog/exec-missing
    pass tests/userprog/exec-bad-ptr
    pass tests/userprog/wait-simple
    pass tests/userprog/wait-twice
    pass tests/userprog/wait-killed
    pass tests/userprog/wait-bad-pid
    pass tests/userprog/multi-recurse
    pass tests/userprog/multi-child-fd
    pass tests/userprog/rox-simple
    pass tests/userprog/rox-child
    pass tests/userprog/rox-multichild
    pass tests/userprog/bad-read
    pass tests/userprog/bad-write
    pass tests/userprog/bad-read2
    pass tests/userprog/bad-write2
    pass tests/userprog/bad-jump
    pass tests/userprog/bad-jump2
    FAIL tests/userprog/no-vm/multi-oom
    pass tests/filesys/base/lg-create
    pass tests/filesys/base/lg-full
    pass tests/filesys/base/lg-random
    pass tests/filesys/base/lg-seq-block
    pass tests/filesys/base/lg-seq-random
    pass tests/filesys/base/sm-create
    pass tests/filesys/base/sm-full
    pass tests/filesys/base/sm-random
    pass tests/filesys/base/sm-seq-block
    pass tests/filesys/base/sm-seq-random
    pass tests/filesys/base/syn-read
    pass tests/filesys/base/syn-remove
    pass tests/filesys/base/syn-write
    1 of 76 tests failed.
    make: *** [check] Error 1

--- src/userprog/exception.c
+++ src/userprog/exception.c
@@ -2,6 +2,7 @@
 #include <inttypes.h>
 #include <stdio.h>
 #include "userprog/gdt.h"
+#include "userprog/syscall.h"
 #include "threads/interrupt.h"
 #include "threads/thread.h"
 
@@ -110,17 +111,18 @@ static void kill(struct intr_frame *f) {
     description of "Interrupt 14--Page Fault Exception (#PF)" in
     [IA32-v3a] section 5.15 "Exception and Interrupt Reference". */
 static void page_fault(struct intr_frame *f) {
-    bool not_present;  /* True: not-present page, false: writing r/o page. */
-    bool write;        /* True: access was write, false: access was read. */
-    bool user;         /* True: access by user, false: access by kernel. */
-    void *fault_addr;  /* Fault address. */
+    // TODO(agf): Remove
+    // bool not_present;  /* True: not-present page, false: writing r/o page. */
+    // bool write;        /* True: access was write, false: access was read. */
+    // bool user;         /* True: access by user, false: access by kernel. */
+    // void *fault_addr;  /* Fault address. */
 
-    /* Obtain faulting address, the virtual address that was accessed to cause
-       the fault.  It may point to code or to data.  It is not necessarily the
-       address of the instruction that caused the fault (that's f->eip).
-       See [IA32-v2a] "MOV--Move to/from Control Registers" and
-       [IA32-v3a] 5.15 "Interrupt 14--Page Fault Exception (#PF)". */
-    asm ("movl %%cr2, %0" : "=r" (fault_addr));
+    // /* Obtain faulting address, the virtual address that was accessed to cause
+    //    the fault.  It may point to code or to data.  It is not necessarily the
+    //    address of the instruction that caused the fault (that's f->eip).
+    //    See [IA32-v2a] "MOV--Move to/from Control Registers" and
+    //    [IA32-v3a] 5.15 "Interrupt 14--Page Fault Exception (#PF)". */
+    // asm ("movl %%cr2, %0" : "=r" (fault_addr));
 
     /* Turn interrupts back on (they were only off so that we could
        be assured of reading CR2 before it changed). */
@@ -129,16 +131,16 @@ static void page_fault(struct intr_frame *f) {
     /* Count page faults. */
     page_fault_cnt++;
 
-    /* Determine cause. */
-    not_present = (f->error_code & PF_P) == 0;
-    write = (f->error_code & PF_W) != 0;
-    user = (f->error_code & PF_U) != 0;
+    // TODO(agf): Remove
+    // /* Determine cause. */
+    // not_present = (f->error_code & PF_P) == 0;
+    // write = (f->error_code & PF_W) != 0;
+    // user = (f->error_code & PF_U) != 0;
 
     // TODO(agf): This is somewhat ad hoc. Also, need to clean up resources.
     // if (!not_present && user) {
     f->eax = -1;
-    printf("%s: exit(-1)\n", thread_name());
-    thread_exit();
+    sys_exit_helper(-1);
 
     // TODO(agf): Remove
     // /* To implement virtual memory, delete the rest of the function
